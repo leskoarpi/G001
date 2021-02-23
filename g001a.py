25cf2a273623973a413b59453947e68845bade2b
leskoarpi <leskoa47@gmail.com>
2021-02-23 20:13:29 +0000

    f

--- g001a.py
+++ g001a.py
@@ -1,1 +1,9 @@
-print("szia")
+print("Adj meg egy számot")
+szám1 = input()
+egészszám1 = int(szám1)
+print("Adj mégegy számot")
+szám2 = input()
+egészszám2 = int(szám2)
+szorzat = egészszám1 * egészszám2
+print("megoldás:")
+print(szorzat)
